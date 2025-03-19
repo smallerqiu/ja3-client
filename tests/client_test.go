40b97c9958e6dfa82972d18de25f822bf174a258
smallerqiu <smallerqiu@gmail.com>
2025-03-19 22:49:10 +0800

    add firefox129  , add safari_18_0 , add safari_17_5

--- tests/client_test.go
+++ tests/client_test.go
@@ -44,17 +44,79 @@ func TestEdge(t *testing.T) {
 }
 
 func TestFirefox(t *testing.T) {
+	t.Log("testing firefox 136")
+	MatchTlsInfo(t, "firefox_136")
+
 	t.Log("testing firefox 135")
-	MatchTlsInfo(t, "firefox 135")
+	MatchTlsInfo(t, "firefox_135")
+
+	t.Log("testing firefox 133")
+	MatchTlsInfo(t, "firefox_133")
+
+	t.Log("testing firefox 132")
+	MatchTlsInfo(t, "firefox_132")
+
+	t.Log("testing firefox 129")
+	MatchTlsInfo(t, "firefox_129")
+
+	t.Log("testing firefox 124")
+	MatchTlsInfo(t, "firefox_124")
+
+	t.Log("testing firefox 123")
+	MatchTlsInfo(t, "firefox_123")
+
+	t.Log("testing firefox 120")
+	MatchTlsInfo(t, "firefox_120")
+
+	t.Log("testing firefox 117")
+	MatchTlsInfo(t, "firefox_117")
+
+	// 117 之前的版本太老了
 }
 
 func TestSafari(t *testing.T) {
-	t.Log("testing safari_ios_16_7")
-	MatchTlsInfo(t, "safari_ios_16_7")
-
-	t.Log("testing safari_18_1")
+	// mac
+	t.Log("testing Safari_18_1")
 	MatchTlsInfo(t, "safari_18_1")
 
+	t.Log("testing Safari_18_0")
+	MatchTlsInfo(t, "safari_18_0")
+
+	t.Log("testing Safari_17_5") // the same 17.0
+	MatchTlsInfo(t, "safari_17_5")
+
+	t.Log("testing Safari_16_0")
+	MatchTlsInfo(t, "safari_16_0")
+
+	t.Log("testing Safari_15_6_1") // the same 15_5
+	MatchTlsInfo(t, "safari_15_6_1")
+
+	t.Log("testing Safari_15_3")
+	MatchTlsInfo(t, "safari_15_3")
+
+	//ios
+
+	t.Log("testing Safari_IOS_18_0")
+	MatchTlsInfo(t, "safari_ios_18_0")
+
+	t.Log("testing Safari_IOS_17_0")
+	MatchTlsInfo(t, "safari_ios_17_0")
+
+	t.Log("testing Safari_IOS_16_7")
+	MatchTlsInfo(t, "safari_ios_16_7")
+
+	t.Log("testing Safari_IOS_16_0")
+	MatchTlsInfo(t, "safari_ios_16_0")
+
+	t.Log("testing Safari_IOS_15_6")
+	MatchTlsInfo(t, "safari_ios_15_6")
+
+	t.Log("testing Safari_IOS_15_5")
+	MatchTlsInfo(t, "safari_ios_15_5")
+
+	//ipad
+	t.Log("testing Safari_Ipad_15_6")
+	MatchTlsInfo(t, "safari_ipad_15_6")
 }
 
 func TestChina(t *testing.T) {
